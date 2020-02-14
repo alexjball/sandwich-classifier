b7ab6474f7f157ce7bbe85936a9188b55febeffc
Alex Ball <alex.ball@cantina.co>
2020-02-13 19:31:41 -0500

    Add instance creation script

---
+++ create-instance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname $BASH_SOURCE)
+
+[ -e .env ] && . .env
+
+gcloud compute instances create $INSTANCE_NAME \
+        --service-account=$SERVICE_ACCOUNT \
+        --zone=$ZONE \
+        --image-family=$IMAGE_FAMILY \
+        --image-project=deeplearning-platform-release \
+        --maintenance-policy=TERMINATE \
+        --accelerator="type=$GPU_TYPE,count=1" \
+        --machine-type=$INSTANCE_TYPE \
+        --boot-disk-size=200GB \
+        --metadata="install-nvidia-driver=True"
+
